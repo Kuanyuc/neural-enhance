d93729b78b2b204ac39790731862c951b20e3d75
jimxing <xyf.jim@gmail.com>
2017-05-01 00:39:57 -0400

    configuration of deep residual blocks and 256 size input that has non-explosion loss training

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # pretrain generator with lower conv2_2 layer
 python3 enhance.py --train "../dataset/**/*.jpg" --loss-save-file=train.latefusion.log --device=cuda \
---model latefusion --epochs=50 \
+--model latefusion --epochs=50 --batch-shape=256 \
 --generator-blocks=8 --generator-filters=128 --generator-residual=0 \
 --perceptual-layer=conv2_2 --smoothness-weight=1e7 --adversary-weight=0.0 \
---batch-size=15 --frame-expanse=1 --learning-rate=1E-4
+--batch-size=15 --frame-expanse=1 --learning-rate=1E-5
 --train-noise=1.0
 
 # train together with discriminator
 python3 enhance.py --train "../dataset/**/*.jpg" --loss-save-file=train.latefusion.log --device=cuda \
 --model latefusion --epochs=2667 --perceptual-layer=conv5_2 \
---smoothness-weight=5e3 --adversary-weight=5e1 \
+--smoothness-weight=5e3 --adversary-weight=5e1 --batch-shape=256 \
 --generator-start=10 --discriminator-start=0 --adversarial-start=10 --discriminator-size=64 \
---batch-size=15 --frame-expanse=1 --learning-rate=1E-4
+--batch-size=15 --frame-expanse=1 --learning-rate=1E-6
