343bdfe8b5af99f0f267497acb48b4d7040f9137
Ubuntu <ubuntu@ip-172-31-32-225.ec2.internal>
2017-05-05 22:31:03 +0000

    use python -- not python3

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 enhance.py --train "../dataset/**/*.jpg" --loss-save-file=train.earlyfusion.log --device=gpu0 \
+python enhance.py --train "../dataset/**/*.jpg" --loss-save-file=train.earlyfusion.log --device=gpu0 \
 --model earlyfusion --train-scales=2 --epochs=1200 --perceptual-layer=conv5_2 --smoothness-weight=2e5 \
 --adversary-weight=1e3 --generator-start=5 --discriminator-start=0 --adversarial-start=5 --discriminator-size=64
