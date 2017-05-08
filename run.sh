77a10bde26ff0f34dd14eed36b56f2752873c0b5
jimxing <xyf.jim@gmail.com>
2017-05-08 12:02:41 -0400

    change to gpu0 (revert back to 0.9.0 theano stable release and 0.2.dev1 lasagne)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 enhance.py --train "../dataset/**/*.jpg" --loss-save-file=train.earlyfusion2.log --device=cuda \
+python3 enhance.py --train "../dataset/**/*.jpg" --loss-save-file=train.earlyfusion2.log --device=gpu0 \
 --model earlyfusion2 --train-scales=2 --epochs=2667 --perceptual-layer=conv5_2 --smoothness-weight=2e4 \
 --adversary-weight=1e3 --generator-start=5 --discriminator-start=0 --adversarial-start=5 --discriminator-size=64
